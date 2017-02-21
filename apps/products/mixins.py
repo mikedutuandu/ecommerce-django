770a4f926e95e0c6a412a67fcba908917059ad7f
mikedutuandu <mikedutuandu@gmail.com>
2017-02-21 11:34:31 +0700

    update code

---
+++ apps/products/mixins.py
@@ -0,0 +1,29 @@
+from django.core.exceptions import ImproperlyConfigured
+
+
+
+
+class FilterMixin(object):
+	filter_class = None
+	search_ordering_param = "ordering"
+
+	def get_queryset(self, *args, **kwargs):
+		try:
+			qs = super(FilterMixin, self).get_queryset(*args, **kwargs)
+			return qs
+		except:
+			raise ImproperlyConfigured("You must have a queryset in order to use the FilterMixin")
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(FilterMixin, self).get_context_data(*args, **kwargs)
+		qs = self.get_queryset()
+		ordering = self.request.GET.get(self.search_ordering_param)
+		if ordering:
+			qs = qs.order_by(ordering)
+		filter_class = self.filter_class
+		if filter_class:
+			f = filter_class(self.request.GET, queryset=qs)
+			context["object_list"] = f.qs
+		return context
+
+
--- apps/products/views.py
+++ apps/products/views.py
@@ -20,6 +20,7 @@ from rest_framework.reverse import reverse as api_reverse
 from rest_framework.views import APIView
 
 # Create your views here.
+from .mixins import FilterMixin
 from .filters import ProductFilter
 from .forms import VariationInventoryFormSet, ProductFilterForm
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -167,32 +168,6 @@ class VariationListView(LoginRequiredMixin, ListView):
 			return redirect("products")
 		raise Http404
 
-
-
-
-
-class FilterMixin(object):
-	filter_class = None
-	search_ordering_param = "ordering"
-
-	def get_queryset(self, *args, **kwargs):
-		try:
-			qs = super(FilterMixin, self).get_queryset(*args, **kwargs)
-			return qs
-		except:
-			raise ImproperlyConfigured("You must have a queryset in order to use the FilterMixin")
-
-	def get_context_data(self, *args, **kwargs):
-		context = super(FilterMixin, self).get_context_data(*args, **kwargs)
-		qs = self.get_queryset()
-		ordering = self.request.GET.get(self.search_ordering_param)
-		if ordering:
-			qs = qs.order_by(ordering)
-		filter_class = self.filter_class
-		if filter_class:
-			f = filter_class(self.request.GET, queryset=qs)
-			context["object_list"] = f.qs
-		return context
 
 
 
