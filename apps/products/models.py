e206511ba4fd6d4fd9192fa9a4d26925a3b4ea30
mikedutuandu <mikedutuandu@gmail.com>
2017-02-23 10:11:09 +0700

    update code

--- apps/products/models.py
+++ apps/products/models.py
@@ -2,162 +2,167 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import post_save
 from django.utils.safestring import mark_safe
-from django.utils.text import slugify
-
-# Create your models here.
-
-class ProductQuerySet(models.query.QuerySet):
-	def active(self):
-		return self.filter(active=True)
+from imagekit.processors import ResizeToFill, Thumbnail
+from imagekit.models import ImageSpecField
+from django.db.models.signals import pre_save
+from config.utils import create_slug
 
 
 class ProductManager(models.Manager):
-	def get_queryset(self):
-		return ProductQuerySet(self.model, using=self._db)
+    def all(self, *args, **kwargs):
+        return super(ProductManager, self).filter(active=True)
 
-	def all(self, *args, **kwargs):
-		return self.get_queryset().active()
-
-	def get_related(self, instance):
-		products_one = self.get_queryset().filter(categories__in=instance.categories.all())
-		products_two = self.get_queryset().filter(default=instance.default)
-		qs = (products_one | products_two).exclude(id=instance.id).distinct()
-		return qs
+    def get_related(self, instance):
+        products_one = super(ProductManager, self).filter(categories__in=instance.categories.all(), active=True)
+        products_two = super(ProductManager, self).filter(default=instance.default, active=True)
+        qs = (products_one | products_two).exclude(id=instance.id).distinct()
+        return qs
 
 
 class Product(models.Model):
-	title = models.CharField(max_length=120)
-	description = models.TextField(blank=True, null=True)
-	price = models.DecimalField(decimal_places=2, max_digits=20)
-	active = models.BooleanField(default=True)
-	categories = models.ManyToManyField('Category', blank=True)
-	default = models.ForeignKey('Category', related_name='default_category', null=True, blank=True)
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    active = models.BooleanField(default=True)
+    categories = models.ManyToManyField('Category', blank=True)
+    default = models.ForeignKey('Category', related_name='default_category', null=True, blank=True)
 
-	objects = ProductManager()
+    objects = ProductManager()
 
-	class Meta:
-		ordering = ["-title"]
+    class Meta:
+        ordering = ["-title"]
 
-	def __unicode__(self): #def __str__(self):
-		return self.title
-	def __str__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title
 
-	def get_absolute_url(self):
-		return reverse("product_detail", kwargs={"pk": self.pk})
+    def __str__(self):
+        return self.title
 
-	def get_image_url(self):
-		img = self.productimage_set.first()
-		if img:
-			return img.image.url
-		return img #None
+    def get_absolute_url(self):
+        return reverse("product_detail", kwargs={"pk": self.pk})
+
+    def get_image_url(self):
+        img = self.productimage_set.first()
+        if img:
+            return img.image.url
+        return img  # None
 
 
+def pre_save_product_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+
+pre_save.connect(pre_save_product_receiver, sender=Product)
 
 
 class Variation(models.Model):
-	product = models.ForeignKey(Product)
-	title = models.CharField(max_length=120)
-	price = models.DecimalField(decimal_places=2, max_digits=20)
-	sale_price = models.DecimalField(decimal_places=2, max_digits=20, null=True, blank=True)
-	active = models.BooleanField(default=True)
-	inventory = models.IntegerField(null=True, blank=True) #refer none == unlimited amount
+    product = models.ForeignKey(Product)
+    title = models.CharField(max_length=120)
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    sale_price = models.DecimalField(decimal_places=2, max_digits=20, null=True, blank=True)
+    active = models.BooleanField(default=True)
+    inventory = models.IntegerField(null=True, blank=True)  # refer none == unlimited amount
 
-	def __unicode__(self):
-		return self.title
-	def __str__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title
 
+    def __str__(self):
+        return self.title
 
-	def get_price(self):
-		if self.sale_price is not None:
-			return self.sale_price
-		else:
-			return self.price
+    def get_price(self):
+        if self.sale_price is not None:
+            return self.sale_price
+        else:
+            return self.price
 
-	def get_html_price(self):
-		if self.sale_price is not None:
-			html_text = "<span class='sale-price'>%s</span> <span class='og-price'>%s</span>" %(self.sale_price, self.price)
-		else:
-			html_text = "<span class='price'>%s</span>" %(self.price)
-		return mark_safe(html_text)
+    def get_html_price(self):
+        if self.sale_price is not None:
+            html_text = "<span class='sale-price'>%s</span> <span class='og-price'>%s</span>" % (
+            self.sale_price, self.price)
+        else:
+            html_text = "<span class='price'>%s</span>" % (self.price)
+        return mark_safe(html_text)
 
-	def get_absolute_url(self):
-		return self.product.get_absolute_url()
+    def get_absolute_url(self):
+        return self.product.get_absolute_url()
 
-	def add_to_cart(self):
-		return "%s?item=%s&qty=1" %(reverse("cart"), self.id)
+    def add_to_cart(self):
+        return "%s?item=%s&qty=1" % (reverse("cart"), self.id)
 
-	def remove_from_cart(self):
-		return "%s?item=%s&qty=1&delete=True" %(reverse("cart"), self.id)
+    def remove_from_cart(self):
+        return "%s?item=%s&qty=1&delete=True" % (reverse("cart"), self.id)
 
-	def get_title(self):
-		return "%s - %s" %(self.product.title, self.title)
-
+    def get_title(self):
+        return "%s - %s" % (self.product.title, self.title)
 
 
 def product_post_saved_receiver(sender, instance, created, *args, **kwargs):
-	product = instance
-	variations = product.variation_set.all()
-	if variations.count() == 0:
-		new_var = Variation()
-		new_var.product = product
-		new_var.title = "Default"
-		new_var.price = product.price
-		new_var.save()
+    product = instance
+    variations = product.variation_set.all()
+    if variations.count() == 0:
+        new_var = Variation()
+        new_var.product = product
+        new_var.title = "Default"
+        new_var.price = product.price
+        new_var.save()
 
 
 post_save.connect(product_post_saved_receiver, sender=Product)
 
 
 def image_upload_to(instance, filename):
-	title = instance.product.title
-	slug = slugify(title)
-	basename, file_extension = filename.split(".")
-	new_filename = "%s-%s.%s" %(slug, instance.id, file_extension)
-	return "products/%s/%s" %(slug, new_filename)
+    return "products/%s" % (filename)
 
 
 class ProductImage(models.Model):
-	product = models.ForeignKey(Product)
-	image = models.ImageField(upload_to=image_upload_to)
+    product = models.ForeignKey(Product)
+    image = models.ImageField(upload_to=image_upload_to)
+    image_thumb1 = ImageSpecField(source='image',
+                                  processors=[ResizeToFill(80, 80)],
+                                  format='JPEG',
+                                  options={'quality': 100})
+    image_thumb2 = ImageSpecField(source='image',
+                                  processors=[ResizeToFill(214, 214)],
+                                  format='JPEG',
+                                  options={'quality': 100})
+    image_thumb3 = ImageSpecField(source='image',
+                                  processors=[ResizeToFill(347, 347)],
+                                  format='JPEG',
+                                  options={'quality': 100})
+    image_thumb4 = ImageSpecField(source='image',
+                                  processors=[ResizeToFill(546, 546)],
+                                  format='JPEG',
+                                  options={'quality': 100})
 
-	def __unicode__(self):
-		return self.product.title
-	def __str__(self):
-		return self.product.title
+    def __unicode__(self):
+        return self.product.title
 
-# Product Category
+    def __str__(self):
+        return self.product.title
 
 
 class Category(models.Model):
-	title = models.CharField(max_length=120, unique=True)
-	slug = models.SlugField(unique=True)
-	description = models.TextField(null=True, blank=True)
-	active = models.BooleanField(default=True)
-	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    title = models.CharField(max_length=120, unique=True)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(null=True, blank=True)
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
-	def __unicode__(self):
-		return self.title
-	def __str__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title
 
+    def __str__(self):
+        return self.title
 
-	def get_absolute_url(self):
-		return reverse("category_detail", kwargs={"slug": self.slug })
-
-
+    def get_absolute_url(self):
+        return reverse("category_detail", kwargs={"slug": self.slug})
 
 
+def pre_save_category_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
 
 
-
-
-
-
-
-
-
-
-
-
+pre_save.connect(pre_save_category_receiver, sender=Category)
--- config/utils.py
+++ config/utils.py
@@ -1,5 +1,5 @@
 from django.utils import timezone
-#from orders.models import UserCheckout
+from django.utils.text import slugify
 
 def jwt_response_payload_handler(token, user, request, *args, **kwargs):
 	data = {
@@ -9,3 +9,16 @@ def jwt_response_payload_handler(token, user, request, *args, **kwargs):
 		#"user_braintree_id": UserCheckout.objects.get(user=user).get_braintree_id
 	}
 	return data
+
+
+def create_slug(instance, new_slug=None):
+	slug = slugify(instance.title)
+	if new_slug is not None:
+		slug = new_slug
+	model_class = type(instance)
+	qs = model_class.objects.filter(slug=slug).order_by("-id")
+	exists = qs.exists()
+	if exists:
+		new_slug = "%s-%s" % (slug, qs.first().id)
+		return create_slug(instance, new_slug=new_slug)
+	return slug
