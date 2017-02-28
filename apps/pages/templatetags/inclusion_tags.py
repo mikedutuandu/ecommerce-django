e4cfe4ba1b4059569b5ac9797703a5ce61b8a999
mikedutuandu <mikedutuandu@gmail.com>
2017-02-28 10:36:16 +0700

    all

--- apps/pages/templatetags/inclusion_tags.py
+++ apps/pages/templatetags/inclusion_tags.py
@@ -1,6 +1,7 @@
 
 from django import template
 from apps.products.models import Product,Category
+from apps.carts.models import Cart,CartItem
 
 register = template.Library()
 
@@ -8,6 +9,7 @@ register = template.Library()
 def hot_products():
     products = Product.objects.filter(hot=True,active=True)[:5]
     return {'products':products}
+
 @register.inclusion_tag('theme_lotus/pages/inclusion_tags/_left_nav.html')
 def left_nav():
     categories = Category.objects.filter(active=True)
@@ -16,9 +18,16 @@ def left_nav():
 @register.inclusion_tag('theme_lotus/pages/inclusion_tags/_latest_post.html')
 def latest_post():
     return {}
+
 @register.inclusion_tag('theme_lotus/pages/inclusion_tags/_head_card.html')
-def head_card():
-    return {}
+def head_card(request):
+    cart_id = request.session.get("cart_id")
+    if cart_id:
+        cart = Cart.objects.get(id= cart_id)
+    else:
+        cart = None
+    return {'cart':cart}
+
 @register.inclusion_tag('theme_lotus/pages/inclusion_tags/_head_search.html')
 def head_search(request):
     categories = Category.objects.filter(active=True)
--- statics/lotus/js/custom.js
+++ statics/lotus/js/custom.js
@@ -64,4 +64,78 @@ jQuery(function () {
 
     })(jQuery, window, document);
 
+
+     /**
+     *  @name  add user
+     *  @description list view
+     *  @version 1.0
+     *  @options
+     *    option
+     *  @events
+     *    event
+     *  @methods
+     *    init
+     */
+    (function ($, window, document, undefined) {
+        var pluginName = "cart";
+
+        // The actual plugin constructor
+        function Plugin(element, options) {
+            this.element = element;
+            this.options = $.extend({}, $.fn[pluginName].defaults, options);
+            this.init();
+        }
+
+        Plugin.prototype = {
+            init: function () {
+                var that = this;
+                that.addCart();
+                that.quantity();
+            },
+            addCart: function () {
+                $('.quantity-container a.btn-primary').click(function(e){
+                    e.preventDefault();
+                    $('form#add-form').submit()
+                });
+            },
+            quantity: function(){
+                $('.cart-quantity .plus i').click(function () {
+                    var currentQuantity = $('.cart-quantity .qty').val();
+                    $('.cart-quantity .qty').val(parseInt(currentQuantity) + 1);
+                });
+                $('.cart-quantity .minus i').click(function () {
+                    var currentQuantity = $('.cart-quantity .qty').val();
+                    if(currentQuantity >1){
+                        $('.cart-quantity .qty').val(parseInt(currentQuantity) - 1);
+                    }
+                });
+                $('.qty').keypress(function(event) {
+                  if ((event.which != 46 || $(this).val().indexOf('.') != -1) && (event.which < 48 || event.which > 57)) {
+                    event.preventDefault();
+                  }
+                });
+
+            }
+
+
+
+        };
+
+        $.fn[pluginName] = function (options) {
+            return this.each(function () {
+                if (!$.data(this, pluginName)) {
+                    $.data(this, pluginName,
+                        new Plugin(this, options));
+                }
+            });
+        };
+        $.fn[pluginName].defaults = {
+            propertyName: 1
+        };
+        $(function () {
+            $('[data-' + pluginName + ']')[pluginName]();
+        });
+
+    })(jQuery, window, document);
+
 });
