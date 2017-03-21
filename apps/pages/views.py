812d059feef7117eeda9a4a947b34a0090887a80
mikedutuandu <mikedutuandu@gmail.com>
2017-03-21 10:59:15 +0000

    all

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -14,14 +14,14 @@ class HomeView(View):
     def get(self, request, *args, **kwargs):
         context = {}
         context['sections'] = []
-        categories = Category.objects.filter(active=True)
+        categories = Category.objects.filter(active=True).order_by('order')
         banners =Banner.objects.filter(active=True,location='home').order_by('order')
         context['banners'] = banners
         for category in categories:
             if self.request.user and (self.request.user.is_superuser or self.request.user.is_staff):
-                products = Product.objects.filter(default=category)[:8].order_by('order')
+                products = Product.objects.filter(default=category)[:8]
             else:
-                products = Product.objects.filter(active=True,default=category)[:8].order_by('order')
+                products = Product.objects.filter(active=True,default=category)[:8]
             context['sections'].append({"category":category,"products":products})
         return render(request,self.template_name,context)
 
